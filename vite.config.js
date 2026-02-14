1962baf6c45afb3b2a2c0c942ff6c4d6348ca865
Van Khai PHAN <vankhaiphan@Khais-MacBook-Pro.local>
2026-02-14 02:48:02 +0100

    Correct path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/love-tree/' : '/', // GitHub Pages base for production, root for dev
+  base: process.env.NODE_ENV === 'production' ? '/apple/' : '/', // GitHub Pages base for production, root for dev
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
